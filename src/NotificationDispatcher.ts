8ee086fcf99a5bf011ac00b7c3befa924ee993fe
Dan Ryan <danryanfl@yahoo.com>
2016-11-01 15:55:36 -0400

    remove protected status from NotificationDispatcher

--- src/NotificationDispatcher.ts
+++ src/NotificationDispatcher.ts
@@ -3,7 +3,7 @@ import { Channel } from 'amqplib/callback_api';
 export class NotificationDispatcher {
   constructor(protected channel: Channel) { }
 
-  protected publish(routingKey: string, content: any, correlationId?: string) {
+  publish(routingKey: string, content: any, correlationId?: string) {
     this.channel.publish('notify', routingKey, new Buffer(JSON.stringify(content)), {
       contentType: 'application/json',
       correlationId
