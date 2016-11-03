7f4c2914a594497fa856e8d994a4e4b6c7ebd7bc
Dan Ryan <danryanfl@yahoo.com>
2016-11-02 22:00:05 -0400

    remove async function from RequestHandler constructor

--- src/RequestClient.ts
+++ src/RequestClient.ts
@@ -3,36 +3,34 @@ import { id } from 'meteor-random';
 
 export abstract class RequestClient {
   protected pending: RequestIndex = {};
-  protected replyTo: string;
 
-  constructor(protected channel: Channel, protected timeoutDelay = 3000) {
-    channel.assertQueue('', { exclusive: true }, (queueErr, ok) => {
-      if (queueErr) throw queueErr;
-      this.replyTo = ok.queue;
-      channel.consume(ok.queue, msg => {
-        console.log('msg received:', msg);
-        if (!this.pending[msg.properties.correlationId]) return;
-        const {cb, timeout} = this.pending[msg.properties.correlationId];
-        clearTimeout(timeout);
-        const {err, result} = JSON.parse(msg.content.toString()) as ResponseContent;
-        cb(err, result);
-      }, { noAck: true });
-    });
+  constructor(protected channel: Channel, protected replyTo: string, protected timeoutDelay = 3000) {
+    channel.consume(replyTo, msg => {
+      if (!this.pending[msg.properties.correlationId]) return;
+      const {cb, timeout} = this.pending[msg.properties.correlationId];
+      clearTimeout(timeout);
+      const {err, res} = JSON.parse(msg.content.toString()) as ResponseContent;
+      cb(err, res);
+    }, { noAck: true });
   }
 
   protected request(routingKey: string, content: any, cb: (err: Error, res?: any) => void) {
     const contentBuffer = new Buffer(JSON.stringify(content));
     const correlationId = id();
-    const timeout = setTimeout(() => {
-      cb(new Error('Timeout Limit Exceeded.'));
-      delete this.pending[correlationId];
-    }, this.timeoutDelay);
-    this.pending[correlationId] = { cb, timeout };
+
+    // publish request
     this.channel.publish('request', routingKey, contentBuffer, {
       contentType: 'application/json',
       replyTo: this.replyTo,
       correlationId
     });
+
+    // register callback and timeout with pending index
+    const timeout = setTimeout(() => {
+      cb(new Error('Timeout Limit Exceeded.'));
+      delete this.pending[correlationId];
+    }, this.timeoutDelay);
+    this.pending[correlationId] = { cb, timeout };
   }
 }
 
@@ -40,7 +38,7 @@ export interface RequestIndex {
   [correlationId: string]: { cb: (err: Error, res?: any) => void, timeout: NodeJS.Timer };
 }
 
-interface ResponseContent {
+export interface ResponseContent {
   err: Error;
-  result?: any;
+  res?: any;
 }
--- src/RequestHandler.ts
+++ src/RequestHandler.ts
@@ -1,23 +1,20 @@
 import { Channel } from 'amqplib/callback_api';
+import { ResponseContent } from './RequestClient';
 const serializerr = require('serializerr');
 
 export abstract class RequestHandler {
   constructor(protected channel: Channel, protected queue: string) {
     channel.consume(queue, msg => {
-      console.log('incoming msg:', msg);
       const routingKey: string = msg.fields.routingKey;
       const content: any = JSON.parse(msg.content.toString());
       const replyTo: string = msg.properties.replyTo;
       const correlationId: string = msg.properties.correlationId;
 
       this.process(routingKey, content, correlationId, (err, res) => {
-        console.log('inside RequestHandler.process cb');
         if (err) err = serializerr(err);
-        console.log(JSON.stringify({ err, res }));
-        console.log(replyTo);
-        console.log(correlationId);
-        const reply = new Buffer(JSON.stringify({ err, res }));
-        channel.sendToQueue(replyTo, reply, { correlationId, contentType: 'application/json' });
+        const responseContent: ResponseContent = { err, res };
+        const responseBuffer = new Buffer(JSON.stringify(responseContent));
+        channel.sendToQueue(replyTo, responseBuffer, { correlationId, contentType: 'application/json' });
         channel.ack(msg);
       });
     });
