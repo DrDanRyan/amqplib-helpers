c72f2fdd0419c4c9a65efdd3643d17d5d1a9aa11
Dan Ryan <danryanfl@yahoo.com>
2016-11-08 15:42:22 -0500

    export serializerr

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './RequestHandler';
 export * from './NotificationHandler';
 export * from './NotificationDispatcher';
 export { Channel, Connection } from 'amqplib/callback_api';
+export const serializerr: (err: Error) => Error = require('serializerr');
