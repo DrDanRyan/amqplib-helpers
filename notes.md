2aedb2ad6d49b5f18ab9e67cbf23ddf7b833fe27
Dan Ryan <danryanfl@yahoo.com>
2016-11-14 10:38:21 -0500

    add some logging to RequestClient

---
+++ notes.md
@@ -0,0 +1,3 @@
+# Logging
+When a request errors, the RequestClient base class should send the error notification.
+The info needed to configure this should be passed into the base class constructor.
--- src/RequestClient.ts
+++ src/RequestClient.ts
@@ -1,10 +1,13 @@
 import { Channel } from 'amqplib/callback_api';
+import { NotificationDispatcher } from './NotificationDispatcher';
 const Random = require('meteor-random');
 
 export abstract class RequestClient {
   protected pending: RequestIndex = {};
+  protected dispatcher: NotificationDispatcher;
 
-  constructor(protected channel: Channel, protected replyTo: string, protected timeoutDelay = 3000) {
+  constructor(protected channel: Channel, protected replyTo: string, protected serviceName: ConstrainDOMString, protected timeoutDelay = 3000) {
+    this.dispatcher = new NotificationDispatcher(channel);
     channel.consume(replyTo, msg => {
       if (!this.pending[msg.properties.correlationId]) return;
       const {cb, timeout} = this.pending[msg.properties.correlationId];
@@ -26,11 +29,21 @@ export abstract class RequestClient {
     });
 
     // register callback and timeout with pending index
+    const loggingCb = (err: Error, res?: any) => {
+      if (err) this.dispatcher.publish('log.error', {
+        service: this.serviceName,
+        role: 'client',
+        routingKey,
+        content,
+        error: err,
+      }, correlationId);
+      cb(err, res);
+    };
     const timeout = setTimeout(() => {
-      cb(new Error('Timeout Limit Exceeded.'));
+      loggingCb(new Error('Timeout Limit Exceeded.'));
       delete this.pending[correlationId];
     }, this.timeoutDelay);
-    this.pending[correlationId] = { cb, timeout };
+    this.pending[correlationId] = { cb: loggingCb, timeout };
   }
 }
 
