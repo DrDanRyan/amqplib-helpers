7e6ad4ad45863c4309bb66b96d4643b4b4a41b25
Dan Ryan <danryanfl@yahoo.com>
2017-02-22 14:34:13 -0500

    require url for connect method instead of read env

--- src/connect.ts
+++ src/connect.ts
@@ -1,7 +1,7 @@
 import { Channel, Connection, connect as openConnection } from 'amqplib/callback_api';
 
-export function connect(cb: (err: Error, result?: {channel: Channel, connection: Connection}) => void) {
-  openConnection(process.env.RABBITMQ_URL, (connectErr, connection) => {
+export function connect(url: string, cb: (err: Error, result?: {channel: Channel, connection: Connection}) => void) {
+  openConnection(url, (connectErr, connection) => {
     if (connectErr) return cb(connectErr, null);
     connection.createChannel((channelErr, channel) => {
       if (channelErr) {
